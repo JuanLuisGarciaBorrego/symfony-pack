0a2daa75ed53d2eb73ffd10c78abee25916519ad
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-08-19 14:09:04 +0200

    browserify + babelify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var browserify = require('browserify');
+var babel = require('babelify');
+var source = require('vinyl-source-stream');
 
 gulp.task('styles', function () {
     gulp.src('app/Resources/assets/sass/**/*.scss')
@@ -9,10 +12,17 @@ gulp.task('styles', function () {
 });
 
 gulp.task('scripts', function () {
+
+    browserify('app/Resources/assets/js/app.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('web/js'));
+
     gulp.src([
         'node_modules/jquery/dist/jquery.min.js',
         'node_modules/materialize-css/dist/js/materialize.min.js',
-        'app/Resources/assets/js/**/*.js'
+        'app/Resources/assets/js/init-template.js'
     ])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('web/js'));
---
+++ app/Resources/assets/js/app.js
@@ -0,0 +1,1 @@
+//Your code EMacScript 2015
