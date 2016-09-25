a3104e41db8195232e3b72cbe52eab13f27f9469
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2016-09-25 18:41:03 +0200

    deleted source-stream

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var source = require('vinyl-source-stream');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var minifyCSS = require('gulp-minify-css');
